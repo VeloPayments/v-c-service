dd3f82374a2f7161f8c81f91bf93a537ee03cbd8
Justin Handville <nanolith@gmail.com>
2023-08-28 01:15:00 -0400

    Added INFO_LOG macro.

--- include/vcservice/log.h
+++ include/vcservice/log.h
@@ -360,6 +360,16 @@ vcservice_log_threshold_level(const vcservice_log* log);
     LOG_WITH_LEVEL(log, VCSERVICE_LOGLEVEL_VERBOSE, __VA_ARGS__)
 
 /**
+ * \brief Log an info message to the given logger.
+ *
+ * \param log           The logger for this operation.
+ * \param ...           A comma separated list of values to append to this log
+ *                      message.
+ */
+#define INFO_LOG(log, ...) \
+    LOG_WITH_LEVEL(log, VCSERVICE_LOGLEVEL_INFO, __VA_ARGS__)
+
+/**
  * \brief Format the given value as hexadecimal if that is supported for this
  * type.
  *
