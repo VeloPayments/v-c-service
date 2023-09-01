d4637bd95d4b8d69b0d46dd66128ecedaf09b8e8
Justin Handville <nanolith@gmail.com>
2023-09-01 18:19:09 -0400

    Added hex and uuid logging examples.

--- examples/stdout_logger/src/main.c
+++ examples/stdout_logger/src/main.c
@@ -13,6 +13,7 @@
 RCPR_IMPORT_allocator_as(rcpr);
 RCPR_IMPORT_psock;
 RCPR_IMPORT_resource;
+RCPR_IMPORT_uuid;
 
 static unsigned int logger_threshold_level();
 
@@ -51,6 +52,7 @@ int main(int argc, char* argv[])
         goto cleanup_alloc;
     }
 
+    /* basic log level display. */
     INFO_LOG(
         log, "The threshold log level has been set to ", threshold_level, ".");
     INFO_LOG(
@@ -61,6 +63,25 @@ int main(int argc, char* argv[])
     CRITICAL_LOG(log, "This is an example critical log.");
     DEBUG_LOG(log, "This is an example debug log.");
     NORMAL_LOG(log, "This is an example normal log.");
+
+    /* we can log hex values. */
+    int myval = 0xDEADBEEF;
+    uint8_t x = '\n';
+    int y = 271;
+
+    NORMAL_LOG(log, "myval = ", LOG_HEX(myval), ".");
+    NORMAL_LOG(log, "x = ", LOG_HEX(x), ", y = ", y, ".");
+
+    /* we can log UUID values. */
+    rcpr_uuid id;
+    retval =
+        rcpr_uuid_parse_string(&id, "5b4bde6e-c7e5-4761-822f-59c489107c54");
+    if (STATUS_SUCCESS != retval)
+    {
+        ERROR_LOG(log, "UUID parse failed.");
+    }
+
+    NORMAL_LOG(log, "id = ", &id, ".");
 
     goto cleanup_log;
 
