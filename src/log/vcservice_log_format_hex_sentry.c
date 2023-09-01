6cd6d58a529b8d249de03e2d1253e55e56fe3566
Justin Handville <nanolith@gmail.com>
2023-09-01 18:05:56 -0400

    impl of vcservice_log_format_hex_sentry.

---
+++ src/log/vcservice_log_format_hex_sentry.c
@@ -0,0 +1,15 @@
+/**
+ * \file log/vcservice_log_format_hex_sentry.c
+ *
+ * \brief Sentry value for the hex log format.
+ *
+ * \copyright 2023 Velo Payments, Inc.  All rights reserved.
+ */
+
+#include <vcservice/log.h>
+
+struct vcservice_log_format_hex
+{
+};
+
+vcservice_log_format_hex vcservice_log_format_hex_sentry;
